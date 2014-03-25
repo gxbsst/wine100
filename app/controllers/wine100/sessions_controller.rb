14bf373daa46c4b8df46d6978a93965e290f3bcc
Weston Wei <weixuhong@rongyi.com>
2014-03-25 09:53:32 +0800

    修复信息未完成调转的问题

--- app/controllers/wine100/sessions_controller.rb
+++ app/controllers/wine100/sessions_controller.rb
@@ -13,19 +13,27 @@ module Wine100
 				return
 			end
 			@user = ::Wine100::User.find_by_email(params[:wine100_user][:email])
-			if @user && @user.authenticate(params[:wine100_user][:password])
-				session[:wine100_user_id] = @user.id
-				if @user.is_completed
-				 redirect_to wine100_accounts_path
-				else
-				 redirect_to complete_wine100_accounts_path
-				end
-
-			else
-				@user = Wine100::User.new
-				flash[:error] = "请输入正确的用户名和密码 Invalid e-mail or password."
-				render :new
-			end
+      if @user && @user.authenticate(params[:wine100_user][:password])
+        session[:wine100_user_id] = @user.id
+        @profile = @user.profile
+        if @profile.present?
+          if !@user.profile.contact_completed?
+            redirect_to edit_wine100_profile_path(@profile, :for => 'contact')
+          elsif !@user.profile.company_completed?
+            redirect_to edit_wine100_profile_path(@profile, :for => 'company')
+          elsif !@user.profile.finance_completed?
+            redirect_to edit_wine100_profile_path(@profile, :for => 'finance')
+          else
+            redirect_to wine100_accounts_path
+          end
+        else
+          redirect_to new_wine100_profile_path(:for => 'contact')
+        end
+      else
+        @user = Wine100::User.new
+        flash[:error] = "请输入正确的用户名和密码 Invalid e-mail or password."
+        render :new
+      end
 		end
 
 		def destroy
